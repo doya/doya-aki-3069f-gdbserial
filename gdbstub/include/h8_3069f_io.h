82c9907ffe49ceb903ab427f2732f903dcaff768
Doya <sohn9086@gmail.com>
2010-12-08 00:15:12 +0900

    Implemented serial_init()

--- gdbstub/include/h8_3069f_io.h
+++ gdbstub/include/h8_3069f_io.h
@@ -5,6 +5,16 @@
 #ifndef _H8_3069F_IO_H_
 #define _H8_3069F_IO_H_
 
+#define SCI_BAUD_9600	(9600)
+#define SCI_BAUD_19200	(19200)
+#define SCI_BAUD_38400	(38400)
+
+#define SCI_CHARLEN_7	(7)
+#define SCI_CHARLEN_8	(8)
+
+#define SCI_PARITY_DISABLED	(0)
+#define SCI_PARITY_ENABLED	(1)
+
 /* SCI1 Registers       Address                                   Register_name              R/W     Default value */
 /* --------------------------------------------------------------------------------- */
 #define SCI1_SMR        (*(volatile unsigned char*)(0xFFFFB8)) /* Serial Mode Register       R/W     0x00          */ 
--- gdbstub/src/serial.c
+++ gdbstub/src/serial.c
@@ -1,4 +1,67 @@
-#include "h8_3069f.h"
+#include "h8_3069f_io.h"
+
+/*
+ * @func serial_init
+ * 
+ * initialize SCI1
+ */
+void serial_init(int baudrate, int charlen, int parity)
+{
+
+	/* シリアルインタフェース設定(初期値でOK) */
+	SCI1_SCMR = (SCI1_SCMR & 0xFE);
+
+	/* 調歩同期式モード設定 */
+	SCI1_SMR = (SCI1_SMR & 0x7F);
+
+	/* LSPファースト設定(?) */
+	SCI1_SCMR = (SCI1_SCMR & 0xF7);
+
+	/* クロックソース設定(ボージェネレータからの内部クロック) */
+	SCI1_SCR = (SCI1_SCR & 0xFC);
+
+	/* ボーレート設定 */
+	switch(baudrate) {
+	case SCI_BAUD_38400:
+		SCI1_SMR = (SCI1_SMR & 0x7F);
+		SCI1_BRR = 15;
+		break;
+	case SCI_BAUD_19200:
+		SCI1_SMR = (SCI1_SMR & 0x7F);
+		SCI1_BRR = 32;
+		break;
+	case SCI_BAUD_9600:
+	default:
+		SCI1_SMR = (SCI1_SMR & 0x7F);
+		SCI1_BRR = 64;
+		break;
+	}
+
+	/* キャラクタレンス設定 */
+	switch(charlen) {
+	case SCI_CHARLEN_7:
+		SCI1_SMR = (SCI1_SMR | 0x40);
+		break;
+	case SCI_CHARLEN_8:
+	default:
+		SCI1_SMR = (SCI1_SMR & 0xBF);
+		break;
+	}
+
+	/* パリティビット設定 */
+	switch(parity) {
+	case SCI_PARITY_ENABLED:
+		SCI1_SMR = (SCI1_SMR | 0x20);
+		break;
+	case SCI_PARITY_DISABLED:
+	default:
+		SCI1_SMR = (SCI1_SMR & 0xDF);
+		break;
+	}
+
+	return;
+}
+
 
 /*
  * @func serial_disable_int
