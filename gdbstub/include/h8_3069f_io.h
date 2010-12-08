9edd88381ac93e8c44481b43cf0a945aac559063
Doya <sohn9086@gmail.com>
2010-12-08 23:55:17 +0900

    Reworked register definitions.

--- gdbstub/include/h8_3069f_io.h
+++ gdbstub/include/h8_3069f_io.h
@@ -5,6 +5,60 @@
 #ifndef _H8_3069F_IO_H_
 #define _H8_3069F_IO_H_
 
+typedef struct sci_registers {
+	/* Serial Mode Register */
+	struct {
+		unsigned C_A    : 1;    /* Communication mode: 0=調歩同期式, 1=クロック同期式 */
+		unsigned CHR    : 1;    /* Character Length: 0=8bit, 1=7bit */
+		unsigned PE     : 1;    /* Parity and check: 0=disable, 1=enable*/
+		unsigned O_E    : 1;    /* Parity mode: 0=even, 1=odd */
+		unsigned STOP   : 1;    /* Stop bit length: 0=1bit, 1=2bit */
+		unsigned MP     : 1;    /* Multiprocessor: 0=disable, 1=enable */
+		unsigned CKS    : 2;    /* Clock select: 0=1, 1=1/4, 2=1/16, 3=1/64*/
+	} SMR;	
+
+	/* Bit Rate Register */
+	unsigned char BRR;
+
+	/* Serial Control Register */
+	struct {
+		unsigned TIE    : 1;    /* Transmit (Data Empty) Interrupt Enable */
+		unsigned RIE    : 1;    /* Receive Interrupt Enable */
+		unsigned TE     : 1;    /* Transmit Enable */
+		unsigned RE     : 1;    /* Receive Enable */
+		unsigned MPIE   : 1;    /* Multi Processor Interrupt Enable */
+		unsigned TEIE   : 1;    /* Transmit End Interrupt Enable */
+		unsigned CKE    : 2;    /* Clock Enable */
+	} SCR; 	
+
+	/* Transmit Data Register */
+	unsigned char TDR;
+
+	/* Serial Status Register */
+	struct {
+		unsigned TDRE   : 1;    /* TDR(Transmit Data Register) Empty*/
+		unsigned RDRF   : 1;    /* RDR(Receive Data Register) Empty */
+		unsigned ORER   : 1;    /* Over Run Error */
+		unsigned FER    : 1;    /* Flaming Error */
+		unsigned PER    : 1;    /* Parity Error */
+		unsigned TEND   : 1;    /* Transmit End */
+		unsigned MPB    : 1;    /* Multi Processor Bit */
+		unsigned MPBT   : 1;    /* Multi Processor Bit Transfer */
+	} SSR; 	
+
+	/* Receive Data Register */
+	unsigned char RDR;
+
+	/* Smart Card Mode Register */
+	struct {
+		unsigned pad0   : 4;
+		unsigned SDIR   : 1;    /* Smartcard Data transfer DIrection */
+		unsigned SINV   : 1;    /* Smartcard Data Invert */
+		unsigned pad1   : 1;
+		unsigned SMIF   : 1;    /* Smartcard Mode Interface Mode: 0=SCI, 1=Smartcard */
+	} SCMR;	
+} sci_registers_t;
+
 #define SCI_BAUD_9600	(9600)
 #define SCI_BAUD_19200	(19200)
 #define SCI_BAUD_38400	(38400)
@@ -15,14 +69,12 @@
 #define SCI_PARITY_DISABLED	(0)
 #define SCI_PARITY_ENABLED	(1)
 
-/* SCI1 Registers       Address                                   Register_name              R/W     Default value */
-/* --------------------------------------------------------------------------------- */
-#define SCI1_SMR        (*(volatile unsigned char*)(0xFFFFB8)) /* Serial Mode Register       R/W     0x00          */ 
-#define SCI1_BRR        (*(volatile unsigned char*)(0xFFFFB9)) /* Bit Rate Register          R/W     0xFF          */ 
-#define SCI1_SCR        (*(volatile unsigned char*)(0xFFFFBA)) /* Serial Control Register    R/W     0x00          */ 
-#define SCI1_TDR        (*(volatile unsigned char*)(0xFFFFBB)) /* Transmit Data Register     R/W     0xFF          */ 
-#define SCI1_SSR        (*(volatile unsigned char*)(0xFFFFBC)) /* Serial Status Register     R/(W)   0x00          */ 
-#define SCI1_RDR        (*(volatile unsigned char*)(0xFFFFBD)) /* Receive Data Register      R       0x84          */ 
-#define SCI1_SCMR       (*(volatile unsigned char*)(0xFFFFBE)) /* Smart Card Mode Register   R/W     0xF2          */ 
+#define SCI_INT_TIE	(1<<0)
+#define SCI_INT_RIE	(1<<1)
+#define SCI_INT_MPIE	(1<<2)
+#define SCI_INT_TEIE	(1<<3)
+
+/* Registers  */
+#define SCI1	(*(volatile sci_registers_t*)(0xFFFFB8))
 
 #endif
--- gdbstub/src/serial.c
+++ gdbstub/src/serial.c
@@ -7,57 +7,64 @@
  */
 void serial_init(int baudrate, int charlen, int parity)
 {
+	/* まず送受信機能をオフにする */
+	SCI1.SCR.TE = 0;
+	SCI1.SCR.RE = 0;
 
 	/* シリアルインタフェース設定(初期値でOK) */
-	SCI1_SCMR = (SCI1_SCMR & 0xFE);
+	SCI1.SCMR.SMIF = 0;
 
 	/* 調歩同期式モード設定 */
-	SCI1_SMR = (SCI1_SMR & 0x7F);
+	SCI1.SMR.C_A = 0;
 
 	/* LSPファースト設定(?) */
-	SCI1_SCMR = (SCI1_SCMR & 0xF7);
+	SCI1.SCMR.SDIR = 0;
 
 	/* クロックソース設定(ボージェネレータからの内部クロック) */
-	SCI1_SCR = (SCI1_SCR & 0xFC);
+	SCI1.SCR.CKE = 0;
 
 	/* ボーレート設定 */
 	switch(baudrate) {
 	case SCI_BAUD_38400:
-		SCI1_SMR = (SCI1_SMR & 0x7F);
-		SCI1_BRR = 15;
+		SCI1.SMR.CKS = 0;
+		SCI1.BRR = 15;
 		break;
 	case SCI_BAUD_19200:
-		SCI1_SMR = (SCI1_SMR & 0x7F);
-		SCI1_BRR = 32;
+		SCI1.SMR.CKS = 0;
+		SCI1.BRR = 32;
 		break;
 	case SCI_BAUD_9600:
 	default:
-		SCI1_SMR = (SCI1_SMR & 0x7F);
-		SCI1_BRR = 64;
+		SCI1.SMR.CKS = 0;
+		SCI1.BRR = 64;
 		break;
 	}
 
 	/* キャラクタレンス設定 */
 	switch(charlen) {
 	case SCI_CHARLEN_7:
-		SCI1_SMR = (SCI1_SMR | 0x40);
+		SCI1.SMR.CHR = 1;
 		break;
 	case SCI_CHARLEN_8:
 	default:
-		SCI1_SMR = (SCI1_SMR & 0xBF);
+		SCI1.SMR.CHR = 0;
 		break;
 	}
 
 	/* パリティビット設定 */
 	switch(parity) {
 	case SCI_PARITY_ENABLED:
-		SCI1_SMR = (SCI1_SMR | 0x20);
+		SCI1.SMR.PE = 1;
 		break;
 	case SCI_PARITY_DISABLED:
 	default:
-		SCI1_SMR = (SCI1_SMR & 0xDF);
+		SCI1.SMR.PE = 0;
 		break;
 	}
+
+	/* 送受信機能をオンにする */
+	SCI1.SCR.TE = 1;
+	SCI1.SCR.RE = 1;
 
 	return;
 }
@@ -66,14 +73,22 @@ void serial_init(int baudrate, int charlen, int parity)
 /*
  * @func serial_disable_int
  * 
- * disable all interrupts of SCI1
+ * disable interrupts of SCI1
  */
-void serial_disable_int(void)
+void serial_disable_int_all(int flag)
 {
-	unsigned char sci1_scr = SCI1_SCR;
-
-	sci1_scr &= 0x33;
-	SCI1_SCR = sci1_scr;
+	if (flag & SCI_INT_TIE) {
+		SCI1.SCR.TIE  = 0;
+	}
+	if (flag & SCI_INT_RIE) {
+		SCI1.SCR.RIE  = 0;
+	}
+	if (flag & SCI_INT_MPIE) {
+		SCI1.SCR.MPIE = 0;
+	}
+	if (flag & SCI_INT_TEIE) {
+		SCI1.SCR.TEIE = 0;
+	}
 
 	return;
 }
@@ -81,14 +96,23 @@ void serial_disable_int(void)
 /*
  * @func serial_enable_int
  * 
- * enable receive interrupts of SCI1
+ * enable interrupts of SCI1
  */
-void serial_enable_int(void)
+void serial_enable_int(int flag)
 {
-	unsigned char sci1_scr = SCI1_SCR;
-
-	sci1_scr |= 0x40;
-	SCI1_SCR = sci1_scr;
+	if (flag & SCI_INT_TIE) {
+		SCI1.SCR.TIE  = 1;
+	}
+	if (flag & SCI_INT_RIE) {
+		SCI1.SCR.RIE  = 1;
+	}
+	if (flag & SCI_INT_MPIE) {
+		SCI1.SCR.MPIE = 1;
+	}
+	if (flag & SCI_INT_TEIE) {
+		SCI1.SCR.TEIE = 1;
+	}
 
 	return;
 }
+
