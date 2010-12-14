0ac8b6c51fd6f4d85fb871e8d4917ca3a0f0d8e4
Doya <sohn9086@gmail.com>
2010-12-15 00:22:25 +0900

    sci_registers_t reworked.

--- gdbstub/include/h8_3069f_io.h
+++ gdbstub/include/h8_3069f_io.h
@@ -7,58 +7,73 @@
 
 typedef struct sci_registers {
 	/* Serial Mode Register */
-	struct {
-		unsigned C_A    : 1;    /* Communication mode: 0=調歩同期式, 1=クロック同期式 */
-		unsigned CHR    : 1;    /* Character Length: 0=8bit, 1=7bit */
-		unsigned PE     : 1;    /* Parity and check: 0=disable, 1=enable*/
-		unsigned O_E    : 1;    /* Parity mode: 0=even, 1=odd */
-		unsigned STOP   : 1;    /* Stop bit length: 0=1bit, 1=2bit */
-		unsigned MP     : 1;    /* Multiprocessor: 0=disable, 1=enable */
-		unsigned CKS    : 2;    /* Clock select: 0=1, 1=1/4, 2=1/16, 3=1/64*/
+	union {
+		struct {
+			volatile unsigned char C_A    : 1;    /* Communication mode: 0=調歩同期式, 1=クロック同期式 */
+			volatile unsigned char CHR    : 1;    /* Character Length: 0=8bit, 1=7bit */
+			volatile unsigned char PE     : 1;    /* Parity and check: 0=disable, 1=enable*/
+			volatile unsigned char O_E    : 1;    /* Parity mode: 0=even, 1=odd */
+			volatile unsigned char STOP   : 1;    /* Stop bit length: 0=1bit, 1=2bit */
+			volatile unsigned char MP     : 1;    /* Multiprocessor: 0=disable, 1=enable */
+			volatile unsigned char CKS    : 2;    /* Clock select: 0=1, 1=1/4, 2=1/16, 3=1/64*/
+		} BIT;	
+		volatile unsigned char BYTE;
 	} SMR;	
 
 	/* Bit Rate Register */
-	unsigned char BRR;
+	union {
+		volatile unsigned char BYTE;
+	} BRR;
 
 	/* Serial Control Register */
 	union {
 		struct {
-			unsigned TIE    : 1;    /* Transmit (Data Empty) Interrupt Enable */
-			unsigned RIE    : 1;    /* Receive Interrupt Enable */
-			unsigned TE     : 1;    /* Transmit Enable */
-			unsigned RE     : 1;    /* Receive Enable */
-			unsigned MPIE   : 1;    /* Multi Processor Interrupt Enable */
-			unsigned TEIE   : 1;    /* Transmit End Interrupt Enable */
-			unsigned CKE    : 2;    /* Clock Enable */
+			volatile unsigned char TIE    : 1;    /* Transmit (Data Empty) Interrupt Enable */
+			volatile unsigned char RIE    : 1;    /* Receive Interrupt Enable */
+			volatile unsigned char TE     : 1;    /* Transmit Enable */
+			volatile unsigned char RE     : 1;    /* Receive Enable */
+			volatile unsigned char MPIE   : 1;    /* Multi Processor Interrupt Enable */
+			volatile unsigned char TEIE   : 1;    /* Transmit End Interrupt Enable */
+			volatile unsigned char CKE    : 2;    /* Clock Enable */
 		} BIT; 	
-		unsigned char BYTE;
+		volatile unsigned char BYTE;
 	} SCR; 	
 
 	/* Transmit Data Register */
-	unsigned char TDR;
+	union {
+		volatile unsigned char BYTE;
+	} TDR;
 
 	/* Serial Status Register */
-	struct {
-		unsigned TDRE   : 1;    /* TDR(Transmit Data Register) Empty*/
-		unsigned RDRF   : 1;    /* RDR(Receive Data Register) Empty */
-		unsigned ORER   : 1;    /* Over Run Error */
-		unsigned FER    : 1;    /* Flaming Error */
-		unsigned PER    : 1;    /* Parity Error */
-		unsigned TEND   : 1;    /* Transmit End */
-		unsigned MPB    : 1;    /* Multi Processor Bit */
-		unsigned MPBT   : 1;    /* Multi Processor Bit Transfer */
+	union {
+		struct {
+			volatile unsigned char TDRE   : 1;    /* TDR(Transmit Data Register) Empty*/
+			volatile unsigned char RDRF   : 1;    /* RDR(Receive Data Register) Empty */
+			volatile unsigned char ORER   : 1;    /* Over Run Error */
+			volatile unsigned char FER    : 1;    /* Flaming Error */
+			volatile unsigned char PER    : 1;    /* Parity Error */
+			volatile unsigned char TEND   : 1;    /* Transmit End */
+			volatile unsigned char MPB    : 1;    /* Multi Processor Bit */
+			volatile unsigned char MPBT   : 1;    /* Multi Processor Bit Transfer */
+		} BIT; 	
+		volatile unsigned char BYTE;
 	} SSR; 	
 
 	/* Receive Data Register */
-	unsigned char RDR;
+	union {
+		volatile unsigned char BYTE;
+	} RDR;
 
 	/* Smart Card Mode Register */
-	struct {
-		unsigned pad0   : 4;
-		unsigned SDIR   : 1;    /* Smartcard Data transfer DIrection */
-		unsigned SINV   : 1;    /* Smartcard Data Invert */
-		unsigned pad1   : 1;
-		unsigned SMIF   : 1;    /* Smartcard Mode Interface Mode: 0=SCI, 1=Smartcard */
+	union {
+		struct {
+			volatile unsigned char pad0   : 4;
+			volatile unsigned char SDIR   : 1;    /* Smartcard Data transfer DIrection */
+			volatile unsigned char SINV   : 1;    /* Smartcard Data Invert */
+			volatile unsigned char pad1   : 1;
+			volatile unsigned char SMIF   : 1;    /* Smartcard Mode Interface Mode: 0=SCI, 1=Smartcard */
+		} BIT;	
+		volatile unsigned char BYTE;
 	} SCMR;	
 } sci_registers_t;
 
@@ -76,6 +91,9 @@ typedef struct sci_registers {
 #define SCI_INT_RIE	(1<<1)
 #define SCI_INT_MPIE	(1<<2)
 #define SCI_INT_TEIE	(1<<3)
+
+#define SCI_SCR_RE	(1<<4)
+#define SCI_SCR_TE	(1<<5)
 
 /* Registers  */
 #define SCI1	(*(volatile sci_registers_t*)(0xFFFFB8))
--- gdbstub/src/serial.c
+++ gdbstub/src/serial.c
@@ -9,77 +9,67 @@ void serial_init(int baudrate, int charlen, int parity)
 {
 	int i;
 
-	/* まず送受信機能をオフにする */
-#if 0
-	SCI1.SCR.BIT.TE = 0;
-	SCI1.SCR.BIT.RE = 0;
-#else
-	SCI1.SCR.BYTE &= ~0x30; /* TE = 0, RE = 0 */
-#endif
+	/* まず送受信機能をオフにする(TE = 0, RE = 0) */
+	SCI1.SCR.BYTE &= ~(SCI_SCR_RE | SCI_SCR_TE);
 
 	/* LSPファースト設定(?) */
-	SCI1.SCMR.SDIR = 0;
+	SCI1.SCMR.BIT.SDIR = 0;
 
 	/* シリアルインタフェース設定(初期値でOK) */
-	SCI1.SCMR.SMIF = 0;
+	SCI1.SCMR.BIT.SMIF = 0;
 
 	/* クロックソース設定(ボージェネレータからの内部クロック) */
 	SCI1.SCR.BIT.CKE = 0;
 
 	/* 調歩同期式モード設定 */
-	SCI1.SMR.C_A = 0;
+	SCI1.SMR.BIT.C_A = 0;
 
 	/* キャラクタレンス設定 */
 	switch(charlen) {
 	case SCI_CHARLEN_7:
-		SCI1.SMR.CHR = 1;
+		SCI1.SMR.BIT.CHR = 1;
 		break;
 	case SCI_CHARLEN_8:
 	default:
-		SCI1.SMR.CHR = 0;
+		SCI1.SMR.BIT.CHR = 0;
 		break;
 	}
 
 	/* パリティビット設定 */
 	switch(parity) {
 	case SCI_PARITY_ENABLED:
-		SCI1.SMR.PE = 1;
+		SCI1.SMR.BIT.PE = 1;
 		break;
 	case SCI_PARITY_DISABLED:
 	default:
-		SCI1.SMR.PE = 0;
+		SCI1.SMR.BIT.PE = 0;
 		break;
 	}
 
 	/* ボーレート設定 */
 	switch(baudrate) {
 	case SCI_BAUD_38400:
-		SCI1.SMR.CKS = 0;
-		SCI1.BRR = 15;
+		SCI1.SMR.BIT.CKS = 0;
+		SCI1.BRR.BYTE = 15;
 		break;
 	case SCI_BAUD_19200:
-		SCI1.SMR.CKS = 0;
-		SCI1.BRR = 32;
+		SCI1.SMR.BIT.CKS = 0;
+		SCI1.BRR.BYTE = 32;
 		break;
 	case SCI_BAUD_9600:
 	default:
-		SCI1.SMR.CKS = 0;
-		SCI1.BRR = 64;
+		SCI1.SMR.BIT.CKS = 0;
+		SCI1.BRR.BYTE = 64;
 		break;
 	}
 
 	/* wait more than 1 bit time */
-	for(i=0; i<10000; i++) {
+	for(i=0; i<1000; i++) {
 		;
 	}
 
-	/* 送受信機能をオンにする */
-#if 0
-	SCI1.SCR.BIT.TE = 1;
-	SCI1.SCR.BIT.RE = 1;
-#else
-	SCI1.SCR.BYTE |= 0x30; /* TE = 1, RE = 1 */
-#endif
+	/* 送受信機能をオンにする(TE = 1, RE = 1) */
+	SCI1.SCR.BYTE |= (SCI_SCR_RE | SCI_SCR_TE);
 
 	return;
 }
@@ -131,18 +121,40 @@ void serial_enable_int(int flag)
 	return;
 }
 
+
+void serial_putchar(char data)
+{
+
+	while(!(SCI1.SSR.BIT.TDRE)) {
+		;
+	}
+
+	SCI1.TDR.BYTE = data;
+	data++;
+	SCI1.SSR.BIT.TDRE = 0;
+
+	while(!(SCI1.SSR.BIT.TEND)) {
+		;
+	}
+}
+
 void serial_puts(char *data)
 {
-	while (data && *data) {
-		while(SCI1.SSR.TDRE != 1) {
+	if (!data) {
+		return;
+	}
+
+	while (*data != '\0') {
+		while(!(SCI1.SSR.BIT.TDRE)) {
 			;
 		}
 
-		SCI1.TDR = *data;
+		SCI1.TDR.BYTE = *data;
 		data++;
+		SCI1.SSR.BIT.TDRE = 0;
 	}
 
-	while(SCI1.SSR.TEND != 1) {
+	while(!(SCI1.SSR.BIT.TEND)) {
 		;
 	}
 	
